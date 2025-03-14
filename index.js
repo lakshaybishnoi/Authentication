09d2bb4e0ab9c543964a4fd5714232b3bd29ed96
Lakshay <bishnoilakshay32@gmail.com>
2025-03-14 12:37:05 +0530

    index.js

--- index.js
+++ index.js
@@ -1,66 +1,89 @@
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "ramdomlakshay"
 const app = express();
 app.use(express.json());
 
 const users = [];
- 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-         
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
-
-app.post("/signup", function(req,res){
+app.post("/signup", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
     users.push({
         username: username,
         password: password
-    })
+    })    
+
     res.json({
-        message: "You are signed in"
+        message: "You are signed up"
     })
 
+    console.log(users)
+    
 })
 
-app.post("/signin", (req, res) => {
+app.post("/signin", function(req, res) {
+    
     const username = req.body.username;
     const password = req.body.password;
 
-    const user = users.find(user => user.username === username && user.password === password);
+    // maps and filter
+    let foundUser = null;
 
-    if (user) {
-        const token = generateToken();
-        user.token = token;
-        res.send({
-            token
+    for (let i = 0; i<users.length; i++) {
+        if (users[i].username == username && users[i].password == password) {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser) {
+        const token = jwt.sign({
+            username: username,
+            password: password,
+            firstname,
+            lastName,
+            courses: []
+        }, JWT_SECRET) ;
+
+        // foundUser.token = token;
+        res.json({
+            token: token
         })
-        console.log(users);
     } else {
         res.status(403).send({
             message: "Invalid username or password"
         })
     }
-});
-
-app.get("/me",function(req,res){
-    const token = req.headers.authorization;
-    const user = users.find(user => user.token === token);
-    if (user){
-        res.send({
-            username: user.username
-        })
-        } else {
-            res.status(403).send({
-                message: "Unauthorized not accisable"
-            })
-    }
+    console.log(users)
 })
-app.listen(3000);
+
+app.get("/me", function(req, res) {
+    const token = req.headers.token // jwt
+    const decodedInformation = jwt.verify(token, JWT_SECRET);  // {username: "harkirat@gmail.com"}
+    const unAuthDecodedinfo = jwt.decode(token,);  // {username: "harkirat@gmail.com"}
+    const username = decodedInformation.username
+    let foundUser = null;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == username)  {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.json({
+            message: "token invalid"
+        })
+    }
+
+
+})
+
+
+app.listen(3000);// that the http server is listening on port 3000
